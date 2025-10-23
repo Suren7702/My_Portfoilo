71ab0fbde804d52fa80964ffe26ec5a31262975e
Suren7702 <suren7702@gmail.com>
2025-10-23 16:37:34 +0530

    Version_1

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Surendhar", "Feelancer", "Web developer"];
+const textArray = ["Surendhar", "Feelancer", "AI-Python Full Stack Developer"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
